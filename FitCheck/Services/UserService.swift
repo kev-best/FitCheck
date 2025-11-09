4c7ea08fde69ba09135c6cc0029467702e9ad602
ewinter2 <elliewinter3@gmail.com>
2025-11-09 11:40:56 -0800

    log in, log out, user profile changes

---
+++ FitCheck/Services/UserService.swift
@@ -0,0 +1,8 @@
+//
+//  UserService.swift
+//  FitCheck
+//
+//  Created by Ellie Winter on 11/9/25.
+//
+
+import Foundation
---
+++ FitCheck/Views/UserLogin/UserLoginView.swift
@@ -0,0 +1,18 @@
+//
+//  UserLoginView.swift
+//  FitCheck
+//
+//  Created by Ellie Winter on 11/9/25.
+//
+
+import SwiftUI
+
+struct UserLoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserLoginView()
+}
